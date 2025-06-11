4a4f52c94963582e20f839a5bb9b4974009ec7e8
Titmouse8 <nikola.sugova@gmail.com>
2025-06-11 14:52:18 +0200

    adding models

--- myproject/Booking/models.py
+++ myproject/Booking/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.category_name
+
+class MenuItem(models.Model):
+    item_name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField(max_length=200)
+
+class Order(models.Model):
+    class StatusChoices(models.TextChoices):
+        PENDING = "pending"
+        CONFIRMED = "confirmed"
+        CANCELLED = "cancelled"
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    status = models.CharField(
+        max_length=10,
+        choices = StatusChoices.choices,
+        default= StatusChoices.PENDING
+    )
+    
+
+class OrderItem(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+    def item_subtotal(self):
+        return self.menu_item.price * self.quantity
+    
+
+    
